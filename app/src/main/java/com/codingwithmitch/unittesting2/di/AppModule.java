64e6442cd85b3d6c73219ebf29b376a5daa8e4ef
DERAOUI Abdelmaoula <mac@MacBook-Pro-de-mac.local>
2023-01-31 17:13:14 +0100

    setup project;add dependencies

---
+++ app/src/main/java/com/codingwithmitch/unittesting2/di/AppModule.java
@@ -0,0 +1,58 @@
+package com.codingwithmitch.unittesting2.di;
+
+import android.app.Application;
+
+import androidx.room.Room;
+
+import com.codingwithmitch.unittesting2.persistence.NoteDao;
+import com.codingwithmitch.unittesting2.persistence.NoteDatabase;
+import com.codingwithmitch.unittesting2.repository.NoteRepository;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+import static com.codingwithmitch.unittesting2.persistence.NoteDatabase.DATABASE_NAME;
+
+@Module
+class AppModule {
+
+    @Singleton
+    @Provides
+    static NoteDatabase provideNoteDatabase(Application application){
+        return Room.databaseBuilder(
+                application,
+                NoteDatabase.class,
+                DATABASE_NAME
+        ).build();
+    }
+
+    @Singleton
+    @Provides
+    static NoteDao provideNoteDao(NoteDatabase noteDatabase){
+        return noteDatabase.getNoteDao();
+    }
+
+
+    @Singleton
+    @Provides
+    static NoteRepository provideNoteRepository(NoteDao noteDao){
+        return new NoteRepository(noteDao);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
